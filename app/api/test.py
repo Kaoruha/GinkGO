c9f7e3f9a3af920b00da330b6954c4db773ac43f
Sunyu <33867159+kaoruha@users.noreply.github.com>
2020-01-31 22:01:10 +0800

    commit test

--- app/api/test.py
+++ app/api/test.py
@@ -10,4 +10,19 @@ def table_generation():
 
 @yp_test.route('/table1')
 def table_generation1():
-    return 'table1'
+    return 'table11111'
+
+
+@yp_test.route('/table2')
+def table_generation2():
+    return 'table22222'
+
+
+@yp_test.route('/table3')
+def table_generation3():
+    return 'table3333'
+
+
+@yp_test.route('/table4')
+def table_generation4():
+    return 'table4444'
