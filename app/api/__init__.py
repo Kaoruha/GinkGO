af0ca4001890cec19302c9d9c08f8f839ecb7071
Yu Sun <809186577@qq.com>
2020-06-28 17:45:11 +0800

    删除多余 api/book.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,6 @@ api模块
 
 from flask import Blueprint
 from app.api.user import yp_user
-from app.api.book import yp_book
 from app.api.test import yp_test
 from app.api.spider import yp_spider
 from app.api.stock import yp_stock
@@ -13,7 +12,6 @@ from app.api.stock import yp_stock
 def create_blueprint():
     bp = Blueprint('bp_api', __name__, url_prefix='/api')
     yp_user.register(bp)
-    yp_book.register(bp)
     yp_test.register(bp)
     yp_spider.register(bp)
     yp_stock.register(bp)
