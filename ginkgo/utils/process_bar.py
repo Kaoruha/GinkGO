b536694674c75222612a31c7e530e19217834137
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-07-18 10:49:01 +0800

    进度条

--- ginkgo/utils/process_bar.py
+++ ginkgo/utils/process_bar.py
@@ -1,5 +1,6 @@
 import time
-    
+
+
 def sleep(self, sleep_second=2):
     """
     线程等待
@@ -8,7 +9,8 @@ def sleep(self, sleep_second=2):
     """
     for i in range(sleep_second * 10):
         t = sleep_second * 10 - (i + 1)
-        rate = (i+1)/(sleep_second*10)
-        _output.write(f'\r还需等待 {t / 10:.1f} 秒 ' + '|' * int(rate*20) + f' {rate*100:.1f}%')
+        rate = (i + 1) / (sleep_second * 10)
+        _output.write(f'\r还需等待 {t / 10:.1f} 秒 ' + '|' * int(rate * 20) +
+                      f' {rate*100:.1f}%')
         time.sleep(.1)
     print('\r\n')
