4c22f2752d116d19cd68fe308b9eb3fdb7892693
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-07-20 19:36:15 +0800

    Create stop_loss.py

---
+++ ginkgo/backtest/risk/stop_loss.py
@@ -0,0 +1,14 @@
+"""
+止损策略
+"""
+from ginkgo.backtest.risk.base_risk import BaseRisk
+
+
+class StopProfit(BaseRisk):
+    """
+    基础止损策略
+
+    :param BaseRisk: []
+    :type BaseRisk: [type]
+    """
+    pass
