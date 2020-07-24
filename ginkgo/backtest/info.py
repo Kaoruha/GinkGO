d984961be704a7581ba741bf4afc06dc8e98ce59
Yu Sun <809186577@qq.com>
2020-07-24 15:38:03 +0800

    增加注释

--- ginkgo/backtest/info.py
+++ ginkgo/backtest/info.py
@@ -16,13 +16,31 @@ class Info(object):
 
 
 class DailyPrice(Info):
+    """
+    日交易数据
+    """
     def __init__(self, data: pd.DataFrame):
+        """
+        初始化日交易数据
+
+        :param data: 包含股票成交日交易级别相关信息，具体格式参见DataPortal部分
+        :type data: pd.DataFrame
+        """
         self.type = InfoType.DailyPrice
         self.data = data
 
 
 class MinutePrice(Info):
+    """
+    分钟交易数据
+    """
     def __init__(self, data: pd.DataFrame):
+        """
+        初始化分钟交易数据
+
+        :param data: 包含股票成交5分钟级相关信息，具体格式参见DataPortal部分
+        :type data: pd.DataFrame
+        """
         self.type = InfoType.MinutePrice
         self.data = data
 
