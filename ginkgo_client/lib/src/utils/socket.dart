181bb4cb59c8641d45ed01d356fa17c65e016fb1
Yu Sun <809186577@qq.com>
2020-08-11 19:14:15 +0800

    Create socket.dart

---
+++ ginkgo_client/lib/src/utils/socket.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'dart:async';
+
+class SocketManage {
+  static String host = '0.0.0.0';
+  static int port = 5102;
+  static Socket mSocket;
+  static Stream<List<int>> mStream;
+
+  static initSocket() async {
+    await Socket.connect(host, port).then((Socket socket) {
+      mSocket = socket;
+      mStream = mSocket.asBroadcastStream(); //多次订阅的流 如果直接用socket.listen只能订阅一次
+    }).catchError((e) {
+      print('connectException:$e');
+      initSocket();
+    });
+  }
+
+  static void addParams(List<int> params) {
+    mSocket.add(params);
+  }
+
+  static void dispos() {
+    mSocket.close();
+  }
+}
