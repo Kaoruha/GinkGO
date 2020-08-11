fe308932d5a5408509d20af51605c0820658ded3
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-08-11 08:32:27 +0800

    replace data_manager with thread_manager

--- ginkgo/api/spider.py
+++ ginkgo/api/spider.py
@@ -1,6 +1,6 @@
 from ginkgo.libs.yellowprint import YellowPrint
 # from ginkgo.data.ip_proxy.proxy_getter import start_thread as proxy_get
-from ginkgo.data.data_manager import kill_all_thread
+# from ginkgo.libs.thread_manager import kill_all_thread
 
 yp_spider = YellowPrint('rp_spider', url_prefix='/spider')
 
@@ -13,5 +13,5 @@ def get_ip_proxy():
 
 @yp_spider.route('/kill')
 def kill_thread():
-    kill_all_thread()
-    return 'All threades killed'
+    # kill_all_thread()
+    return 'All threads killed'
