c4e2827adf7a399414c55263656b86a1ba815589
Yu Sun <809186577@qq.com>
2020-08-11 19:14:06 +0800

    Update engine.dart

--- ginkgo_client/lib/src/api/engine.dart
+++ ginkgo_client/lib/src/api/engine.dart
@@ -2,8 +2,8 @@ import 'package:ginkgo_client/src/utils/dio.dart';
 
 String url_prefix = '/engine';
 
-void EngineRun(){
-  String url = '/start';
+void EngineBoost(){
+  String url = '/boost';
   String tar_url = url_prefix + url;
   ginkgo_dio.post(tar_url);
 }
@@ -13,3 +13,16 @@ void EngineSleep(){
   String tar_url = url_prefix + url;
   ginkgo_dio.post(tar_url);
 }
+
+void EngineResume(){
+  String url = '/resume';
+  String tar_url = url_prefix + url;
+  ginkgo_dio.post(tar_url);
+}
+
+void InfoInjection(){
+  String url = '/info_injection';
+  String tar_url = url_prefix + url;
+  ginkgo_dio.post(tar_url);
+}
+
