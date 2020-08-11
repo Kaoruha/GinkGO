bfed4734ba3bfeeda2d6f2683058a56cbb235b99
Yu Sun <809186577@qq.com>
2020-08-11 19:14:23 +0800

    Create test_strategy.py

---
+++ ginkgo/backtest/strategies/test_strategy.py
@@ -0,0 +1,20 @@
+"""
+测试策略策略
+"""
+from ginkgo.backtest.strategies.base_strategy import BaseStrategy
+from ginkgo.backtest.events import SignalEvent
+from pandas import DataFrame
+
+
+class TestStrategy(BaseStrategy):
+
+    def check(self):
+        for code in self.data['daily']:
+            latest_close = self.data['daily'][code].iloc[-1]['close']
+            if latest_close > self.data['daily'][code].iloc[-2]['close']:
+                signal = SignalEvent(date=self.data['daily'][code].iloc[-1]['date'],
+                                     code=code, buy_or_sell='BUY'
+                                     )
+                return signal
+            print(f'latest_close: {latest_close}')
+            print('=========================')
