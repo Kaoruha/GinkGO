c85ea2716e57076a5ce8ebc2f3d333f678010bc7
Yu Sun <809186577@qq.com>
2020-08-11 19:14:20 +0800

    Create engine_page.dart

---
+++ ginkgo_client/lib/src/pages/engine_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../api/stock.dart';
+import '../api/engine.dart';
+
+class EnginePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello_Flutter',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Ginkgo Test')),
+        body: Row(
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.lightBlue[200],
+              onPressed: () {
+                EngineBoost();
+              },
+              child: Text('引擎开启'),
+            ),
+            RaisedButton(
+              color: Colors.lightBlue[200],
+              onPressed: () {
+                EngineSleep();
+              },
+              child: Text('引擎休眠'),
+            ),
+            RaisedButton(
+              color: Colors.lightBlue[300],
+              onPressed: () {
+                InfoInjection();
+              },
+              child: Text('喂数据'),
+            ),
+            RaisedButton(
+              color: Colors.lightBlue[100],
+              onPressed: () {
+                StockDataUpdate();
+              },
+              child: Text('数据更新'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
