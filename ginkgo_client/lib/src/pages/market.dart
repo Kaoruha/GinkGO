83cb306882131bf9783836df9c8205b36c50dcca
Yu Sun <809186577@qq.com>
2020-10-21 16:07:40 +0800

    Update market.dart

--- ginkgo_client/lib/src/pages/market.dart
+++ ginkgo_client/lib/src/pages/market.dart
@@ -15,7 +15,7 @@ class MarketState extends State<Market> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 7, vsync: this);
+    tabController = TabController(length: 11, vsync: this);
   }
 
   @override
@@ -28,19 +28,31 @@ class MarketState extends State<Market> with SingleTickerProviderStateMixin {
               text: "Ginkgo",
             ),
             Tab(
+              text: "Future",
+            ),
+            Tab(
               text: "CN",
             ),
             Tab(
               text: "US",
             ),
             Tab(
-              text: "Future",
-            ),
-            Tab(
               text: "SG",
             ),
             Tab(
               text: "JPN",
+            ),
+            Tab(
+              text: "UK",
+            ),
+            Tab(
+              text: "RUS",
+            ),
+            Tab(
+              text: "IND",
+            ),
+            Tab(
+              text: "AUS",
             ),
             Tab(
               text: "Others",
@@ -50,16 +62,21 @@ class MarketState extends State<Market> with SingleTickerProviderStateMixin {
           isScrollable:true,
           labelStyle:TextStyle(color: Colors.white),
           indicatorColor: Colors.white,
+          indicatorSize: TabBarIndicatorSize.label,
         ),
       ),
       body: TabBarView(
         children: [
           MarketGinkgo(),
+          MarketFutures(),
           MarketCN(),
           MarketUS(),
-          MarketFutures(),
           Center(child: Text('新加坡')),
           Center(child: Text('日本')),
+          Center(child: Text('英国')),
+          Center(child: Text('俄罗斯')),
+          Center(child: Text('印度')),
+          Center(child: Text('澳大利亚')),
           Center(child: Text('其他')),
         ],
         controller: tabController,
