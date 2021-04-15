10403f6fb7535405d89b4df33e3086bd19d21662
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2021-04-16 07:20:09 +0800

    add trendfollow

--- ginkgo_server/backtest/analyzer/base_analyzer.py
+++ ginkgo_server/backtest/analyzer/base_analyzer.py
@@ -1,13 +1,25 @@
 """
 分析评价基类
+
+净值曲线 equity curve
+业绩基准 benchmark
+夏普比率 过去一段时间段平均年华收益率减去所谓的无风险收益率，再将结果除以该器件收益率的标准差
+索提诺比率 与夏普比率类似，但是只计量负向的波动
+最大亏损弥补时间 将历史最大回撤除以年化收益率
+头寸平均持仓时间
+盈利头寸平均持仓时间/亏损头寸平均持仓时间
+总交易笔数
+盈利交易笔数/亏损交易笔数
+
 """
 from ginkgo_server.backtest.event_engine import EventEngine
 
+
 class BaseAnalyzer(object):
     def __init__(self):
-        self._init_date=''
-        self._current_date=''
-        self._target = 'sh.'
+        self._init_date = ""
+        self._current_date = ""
+        self._target = "sh."
         self._engine = None
         self._init_capital = 0
         self._capital = 0
