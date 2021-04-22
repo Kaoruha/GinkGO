cca2af200d259c95d4fe8e0fb1a7a6da96d6f860
Sunyu <809186577@qq.com>
2021-04-22 18:19:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from ginkgo_server.backtest.sizer.risk_avg_sizer import RiskAVGSizer
+from ginkgo_server.util.ATR import CAL_ATR
 
-
-r = RiskAVGSizer()
+r = RiskAVGSizer(risk_factor=10000)
 print(r._risk_factor)
 print(r._init_capital)
 print(r)
+
+m = r.cal(10000, "sh.000091", "2020-01-22")
+print(m)
