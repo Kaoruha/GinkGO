d46086719623a82b728f43f35dd2edda21893622
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2021-04-26 06:32:23 +0800

    Update simulate_matcher.py

--- ginkgo_server/backtest/matcher/simulate_matcher.py
+++ ginkgo_server/backtest/matcher/simulate_matcher.py
@@ -1,6 +1,7 @@
 import random
 from ginkgo_server.backtest.matcher.base_matcher import BaseMatcher
-from ginkgo_server.backtest.events import DealType
+from ginkgo_server.backtest.events import DealType, FillEvent
+from ginkgo_server.data.ginkgo_mongo import ginkgo_mongo as gm
 
 
 class SimulateMatcher(BaseMatcher):
@@ -14,10 +15,11 @@ class SimulateMatcher(BaseMatcher):
         slippage=0.02,
     ):
         super(SimulateMatcher, self).__init__(
-            stamp_tax_rate,
-            transfer_fee_rate,
-            commission_rate,
-            min_commission,
+            name="回测模拟成交",
+            stamp_tax_rate=stamp_tax_rate,
+            transfer_fee_rate=transfer_fee_rate,
+            commission_rate=commission_rate,
+            min_commission=min_commission,
         )
         self._slippage = slippage
 
@@ -28,6 +30,9 @@ class SimulateMatcher(BaseMatcher):
         min_comm = self._min_commission
         s = f"回测模拟成交，当前印花税：「{stamp}」，过户费：「{trans}」，交易佣金：「{comm}」，最小交易佣金：「{min_comm}」"
         return s
+
+    def send_order(self, order):
+        self._match_list.append(order)
 
     def try_match(self, order, broker, price):
         """
@@ -44,8 +49,7 @@ class SimulateMatcher(BaseMatcher):
 
         # 尝试成交
         if order.deal == DealType.BUY:
-            r = (random.random() * 2 - 1) * self._slippage
-            p = float(price.loc[0].open) * (1 + r)
+            p = float(price.loc[0].open)
             bussiness_volume = p * order.volume
             fee = self.fee_cal(bussiness_volume=bussiness_volume, deal_type=order.deal)
             if broker._capitial <= (bussiness_volume + fee):
@@ -56,7 +60,25 @@ class SimulateMatcher(BaseMatcher):
             print(
                 f"预计成交量：{order.volume}，预计成交金额：{bussiness_volume}，税费：{fee}，当前现金：{broker._capitial}"
             )
-            # broker.freeze_money(bussiness_volume + fee)
-            print(broker)
+            broker.freeze_money(bussiness_volume + fee)
+        elif order.deal == DealType.SELL:
+            if order.code not in broker.position:
+                print(f"未持有{order.code}")
+            broker.position[order.code].per_sell(order.volume)
+            order.volume = broker.position[order.code].freeze
 
-        # elif order.deal == DealType.SELL:
+        order.date = price.loc[0].date
+
+        self.send_order(order)
+
+    def get_result(self):
+        print("尝试获取结果")
+        result = []
+        for i in self._match_list:
+            r = (random.random() * 2 - 1) * self._slippage
+            p_df = gm.get_dayBar_by_mongo(
+                code=i.code, start_date=i.date, end_date=i.date
+            )
+            p = float(p_df.loc[0].open) * (1 + r)
+            # TODO 需要考虑涨跌停
+            print(p)
