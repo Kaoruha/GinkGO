40183fbead3ddfe78aa84080ed468ebe5161eaa1
Sunyu <809186577@qq.com>
2021-04-28 17:59:48 +0800

    TODO  去除时间上的校验，只根据当前的价格信息尝试成交

--- ginkgo_server/backtest/matcher/simulate_matcher.py
+++ ginkgo_server/backtest/matcher/simulate_matcher.py
@@ -3,6 +3,7 @@ from ginkgo_server.backtest.matcher.base_matcher import BaseMatcher
 from ginkgo_server.backtest.events import FillEvent
 from ginkgo_server.backtest.enums import DealType
 from ginkgo_server.data.ginkgo_mongo import ginkgo_mongo as gm
+from ginkgo_server.backtest.price import DayBar, Min5Bar
 
 
 class SimulateMatcher(BaseMatcher):
@@ -35,22 +36,23 @@ class SimulateMatcher(BaseMatcher):
     def send_order(self, order):
         self._match_list.append(order)
 
-    def try_match(self, order, broker, price):
+    def try_match(self, order, broker, price: DayBar):
         """
         尝试撮合
         """
         # 检查价格信息是否为空
-        if price.shape[0] != 1:
-            print(f"今日{order.code} 无价格信息，可能休市")
-            return
+        # 修改了Price的格式，好像不需要检查是否为空了
+        # if price.shape[0] != 1:
+        #     print(f"今日{order.code} 无价格信息，可能休市")
+        #     return
         # 检查价格信息的日期是否在订单事件日期之后
-        if str(price.loc[0].date) <= str(order.date):
+        if str(price.data.date) <= str(order.date):
             print("需要在订单事件生成的第二天才可以进行撮合尝试")
             return order
 
         # 尝试成交
         if order.deal == DealType.BUY:
-            p = float(price.loc[0].open) * 1.1
+            p = float(price.data.open) * 1.1
             bussiness_volume = p * order.volume
             fee = self.fee_cal(bussiness_volume=bussiness_volume, deal_type=order.deal)
             if broker._capitial <= (bussiness_volume + fee):
@@ -69,7 +71,8 @@ class SimulateMatcher(BaseMatcher):
             broker.position[order.code].per_sell(order.volume)
             order.volume = broker.position[order.code].freeze
 
-        order.date = price.loc[0].date
+        order.date = price.data.date
+        order.source = f"{self._name} 通过初步校验，模拟发出下单命令，等待返回下单结果"
 
         self.send_order(order)
 
@@ -80,28 +83,28 @@ class SimulateMatcher(BaseMatcher):
         交易成功会返回FillEvent，交易失败会返回失败的FillEvent与日期更新后的Order
         """
         result = []
-        price = price.loc[0]
+        price = price.data
         for i in self._match_list:
             # 休市或者停牌的处理
-            if price.shape[0] == 0:
-                i.source = f"{price.date} 无价格信息，可能休市或停牌，模拟成交类重新推送"
-                i.date = price.date
-                f = FillEvent(
-                    deal=i.deal,
-                    date=price.date,
-                    code=i.code,
-                    price=0,
-                    volume=0,
-                    source="无价格信息",
-                    fee=0,
-                    remain=i.freeze if i.deal == DealType.BUY else i.volume,
-                    done=False,
-                )
-                result.append(f)
-                result.append(i)
-                continue
+            # if price.shape[0] == 0:
+            #     i.source = f"{price.date} 无价格信息，可能休市或停牌，模拟成交类重新推送"
+            #     i.date = price.date
+            #     f = FillEvent(
+            #         deal=i.deal,
+            #         date=price.date,
+            #         code=i.code,
+            #         price=0,
+            #         volume=0,
+            #         source="无价格信息",
+            #         fee=0,
+            #         remain=i.freeze if i.deal == DealType.BUY else i.volume,
+            #         done=False,
+            #     )
+            #     result.append(f)
+            #     result.append(i)
+            #     continue
 
-            pct_chg = float(price["pct_change"])
+            pct_chg = float(price["pct_chg"])
 
             # 涨跌停处理
             if abs(pct_chg) >= 9.5:
