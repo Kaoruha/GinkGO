98c05f097a1bedb2190354da573c5640b64c2a04
Sunyu <809186577@qq.com>
2021-05-06 18:38:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,28 +12,26 @@ from ginkgo_server.backtest.postion import Position
 from ginkgo_server.data.ginkgo_mongo import ginkgo_mongo as gm
 from ginkgo_server.backtest.matcher.simulate_matcher import SimulateMatcher
 from ginkgo_server.backtest.strategy.test_strategy import TestStrategy
+from ginkgo_server.backtest.strategy.trend_follow import TrendFollow
 from ginkgo_server.backtest.price import DayBar
 
 r = RiskAVGSizer(base_factor=50)
 
-# engine = EventEngine()
-# broker = T1Broker(engine=engine)
-# engine.register(EventType.Market, broker.market_handler)
-# engine.register(EventType.Signal, broker.signal_handler)
-# engine.register(EventType.Order, broker.order_handler)
-# engine.register(EventType.Fill, broker.fill_handler)
+engine = EventEngine()
+broker = T1Broker(engine=engine)
+engine.register(EventType.Market, broker.market_handler)
+engine.register(EventType.Signal, broker.signal_handler)
+engine.register(EventType.Order, broker.order_handler)
+engine.register(EventType.Fill, broker.fill_handler)
 # tf_strategy = TestStrategy()
-# broker.strategy_register(tf_strategy)
-# broker.sizer_register(sizer=r)
-# broker.get_cash(2000)
-# matcher = SimulateMatcher()
-# broker.matcher_register(matcher=matcher)
+tf_strategy = TrendFollow(short_term=50, long_term=100, factor=50)
+broker.strategy_register(tf_strategy)
+broker.sizer_register(sizer=r)
+broker.get_cash(2000)
+matcher = SimulateMatcher()
+broker.matcher_register(matcher=matcher)
 
 
-# pdata = gm.get_dayBar_by_mongo(code="sz.000725")
-# engine.feed(pdata)
-# engine.start()
-count = 0
-for i in range(5):
-    count += i
-print(count)
+pdata1 = gm.get_dayBar_by_mongo(code="sz.000725", start_date="2018-01-01")
+engine.feed(pdata1)
+engine.start()
