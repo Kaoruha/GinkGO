e3adf0e1c2593337ec077a03f3eddd56779ccb11
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2021-05-09 22:07:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,12 +13,12 @@ from ginkgo.data.ginkgo_mongo import ginkgo_mongo as gm
 from ginkgo.backtest.matcher.simulate_matcher import SimulateMatcher
 from ginkgo.backtest.strategy.test_strategy import TestStrategy
 from ginkgo.backtest.strategy.trend_follow import TrendFollow
+from ginkgo.backtest.strategy.profit_loss_limit import ProfitLossLimit
 from ginkgo.backtest.price import DayBar
 from ginkgo.util.stock_filter import remove_index
 from ginkgo.backtest.painter.candle import CandlePainter
 
-r = RiskAVGSizer(base_factor=8000)
-
+# 引擎事件注册
 engine = EventEngine()
 broker = T1Broker(init_capitial=100000, engine=engine)
 engine.register(EventType.Market, broker.market_handler)
@@ -26,21 +26,33 @@ engine.register(EventType.Signal, broker.signal_handler)
 engine.register(EventType.Order, broker.order_handler)
 engine.register(EventType.Fill, broker.fill_handler)
 # tf_strategy = TestStrategy()
+# 策略挂载
 short_ = 5
 long_ = 20
-tf_strategy = TrendFollow(short_term=short_, long_term=long_, gap_count=2)
+tf_strategy = TrendFollow(short_term=short_, long_term=long_, gap_count=3)
 broker.strategy_register(tf_strategy)
+
+pll_strategy = ProfitLossLimit(limit=(10, 5))
+broker.strategy_register(pll_strategy)
+
+# 开仓策略
+r = RiskAVGSizer(base_factor=200)
 broker.sizer_register(sizer=r)
+
+# 模拟撮合
 matcher = SimulateMatcher()
 broker.matcher_register(matcher=matcher)
 
+# 绘图
 painter = CandlePainter(mav=(short_, long_))
 broker.painter_register(painter)
 
+# 准备数据
 code_list = remove_index()
 code = code_list.sample(n=1).iloc[0].code
+pdata1 = gm.get_dayBar_by_mongo(code="sh.600377", start_date="2018-01-01")
 
-pdata1 = gm.get_dayBar_by_mongo(code="sz.300359", start_date="2018-05-01")
+# 喂数据，启动
 engine.feed(pdata1)
 engine.start()
 import matplotlib.pyplot as plt
