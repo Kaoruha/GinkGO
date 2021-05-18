00d132e90801ab32c6a8113b1a56564c265038cc
Sunyu <809186577@qq.com>
2021-05-18 16:14:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ broker.painter_register(painter)
 # 准备数据
 code_list = remove_index()
 code = code_list.sample(n=1).iloc[0].code
-pdata1 = gm.get_dayBar_by_mongo(code=code, start_date="2019-01-01")
+pdata1 = gm.get_dayBar_by_mongo(code=code, start_date="2018-01-01")
 
 # 喂数据，启动
 engine.feed(pdata1)
