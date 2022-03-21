85dbbe324f41d9dbc7997f0db11765a2266d4941
Kaoru <Kaoru@codefever.cn>
2022-03-22 02:32:38 +0800

    Create trade.py

---
+++ src/backtest/trade.py
@@ -0,0 +1,16 @@
+"""
+Author: Kaoru
+Date: 2022-03-22 02:09:59
+LastEditTime: 2022-03-22 02:10:00
+LastEditors: Kaoru
+Description: Be stronger,be patient,be confident and never say die.
+FilePath: /Ginkgo/src/backtest/trade.py
+What goes around comes around.
+"""
+
+
+class Trade(object):
+    """ """
+
+    def __init__(self, trade_status) -> None:
+        self.trade_status = trade_status
