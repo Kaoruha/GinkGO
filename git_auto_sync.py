faf87d87a92d609ac9ab11dc53f6fd48ef06fa6b
Kaoru <809186577@qq.com>
2022-05-08 02:00:29 +0800

    adjust github sync

--- git_auto_sync.py
+++ git_auto_sync.py
@@ -17,10 +17,17 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     com = args.comment
-    print(com)
+    print(f"Your comment is {com}")
     os.system("git add .")
     os.system(f'git commit -m "{com}"')
     print("CodeFever push.")
+
+    # 换回CodeFever源头
+    print("Changing Origin to CodeFever")
+    os.system("git remote rm origin")
+    os.system(f"git remote add origin {codefever_url}")
+    os.system('git push -u origin "main"')
+    print("CodeFever push")
 
     # 换Github源头
     print("Changing Origin to Github")
@@ -29,9 +36,6 @@ if __name__ == "__main__":
     os.system('git push -u origin "master"')
     print("Github push")
 
-    # 换回CodeFever源头
     print("Changing Origin to CodeFever")
     os.system("git remote rm origin")
     os.system(f"git remote add origin {codefever_url}")
-    os.system('git push -u origin "main"')
-    print("CodeFever push")
