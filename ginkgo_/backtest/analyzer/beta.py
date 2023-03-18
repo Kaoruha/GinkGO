02d26a804cf20c4324db092e2ea5b1ed163a13a9
kaoru <809186577@qq.com>
2023-03-19 01:37:30 +0800

    remove the old code

--- ginkgo_/backtest/analyzer/beta.py
+++
@@ -1,7 +0,0 @@
-"""
-Date: 2022-03-01 15:22:18
-Author: Suny
-LastEditors: Suny
-LastEditTime: 2022-03-01 15:22:18
-FilePath: \GinkGO\ginkgo\backtest\analyzer\beta.py
-"""
--- ginkgo_/util/logger_formatter.py
+++
@@ -1,9 +0,0 @@
-import tornado.log
-
-
-class LogFormatter(tornado.log.LogFormatter):
-    def __init__(self):
-        super(LogFormatter, self).__init__(
-            fmt=
-            "[%(levelname)s] [%(asctime)s %(filename)s:%(funcName)s]: %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S")
