a1f62ee1e7533cc3c88c990d4525129135c4cb6e
Yu <suny@rc.inesa.com>
2023-03-28 05:56:48 -0400

    update

---
+++ ginkgo/base.py
@@ -0,0 +1,2 @@
+class GinkgoBase(object):
+    pass
---
+++ ginkgo/data/models/tick.py
@@ -0,0 +1,22 @@
+from ginkgo.data.models.base_model import BaseModel
+from ginkgo.enums import DIRECTION_TYPES, ORDER_TYPES, ORDERSTATUS_TYPES, SOURCE_TYPES
+from sqlalchemy import Column, String, Integer, DECIMAL
+from ginkgo.libs.ginkgo_conf import GINKGOCONF
+from clickhouse_sqlalchemy import engines
+from sqlalchemy_utils import ChoiceType
+
+
+class Tick(BaseModel):
+    __abstract__ = False
+    __tablename__ = "tick"
+
+    if GINKGOCONF.DBDRIVER == "clickhouse":
+        __table_args__ = (engines.Memory(),)
+
+    code = Column(String(25), default="ginkgo_test_code")
+    direction = Column(ChoiceType(DIRECTION_TYPES, impl=Integer()), default=1)
+    order_type = Column(ChoiceType(ORDER_TYPES, impl=Integer()), default=1)
+    status = Column(ChoiceType(ORDERSTATUS_TYPES, impl=Integer()), default=1)
+    source = Column(ChoiceType(SOURCE_TYPES, impl=Integer()), default=1)
+    quantity = Column(Integer, default=0)
+    price = Column(DECIMAL(9, 2), default=0)
