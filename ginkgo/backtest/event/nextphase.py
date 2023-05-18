df336094a0f742c0517f43965831a09dd74cded2
Yu <suny@rc.inesa.com>
2023-05-18 15:01:41 +0800

    add next event

---
+++ ginkgo/backtest/event/nextphase.py
@@ -0,0 +1,15 @@
+from ginkgo.backtest.event.base_event import EventBase
+from ginkgo.enums import EVENT_TYPES
+from ginkgo.backtest.order import Order
+from ginkgo.data.ginkgo_data import GINKGODATA
+from ginkgo.libs.ginkgo_pretty import base_repr
+from ginkgo.libs import GINKGOLOGGER as gl
+
+
+class EventNextPhase(EventBase):
+    def __init__(self, order_id=None, *args, **kwargs) -> None:
+        super(EventOrderFill, self).__init__(*args, **kwargs)
+        self.event_type = EVENT_TYPES.NEXTPHASE
+
+    def __repr__(self):
+        return base_repr(self, EventOrderFill.__name__, 16, 60)
