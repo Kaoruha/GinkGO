a896f60d5aa92dac2ac514d78f8747a08d52f54d
kaoru <809186577@qq.com>
2023-05-22 22:07:27 +0800

    maybe fixd daybar update

--- for_test.py
+++ for_test.py
@@ -7,10 +7,17 @@ from ginkgo.libs.ginkgo_conf import GINKGOCONF
 gd = GinkgoData()
 
 
-t = gd.get_bar_lastdate("sh.000002", FREQUENCY_TYPES.DAY)
-print(t)
+# t = gd.get_bar_lastdate("sh.000002", FREQUENCY_TYPES.DAY)
+# print(t)
 
 
 # rs = gd.bs.fetch_cn_stock_daybar("sh.000066", "2000-01-01", "2023-01-01")
 # rs = gd.bs.fetch_cn_stock_daybar("sh.600990", "2004-05-10", "2012-07-27")
 # print(rs)
+
+
+from ginkgo.backtest.matchmaknig.sim_matchmaking import MatchMaking_Sim
+
+m = MatchMaking_Sim()
+for i in range(20):
+    print(m.ratio)
--- ginkgo/backtest/matchmaknig/sim_matchmaking.py
+++ ginkgo/backtest/matchmaknig/sim_matchmaking.py
@@ -1,6 +1,7 @@
 import datetime
+import random
 from ginkgo.libs import datetime_normalize
-from ginkgo.enums import EVENT_TYPES
+from ginkgo.enums import EVENT_TYPES, ATTITUDE_TYPES
 from ginkgo.backtest.event import EventPriceUpdate
 from ginkgo.libs import GINKGOLOGGER as gl
 
@@ -8,11 +9,28 @@ from ginkgo.libs import GINKGOLOGGER as gl
 class MatchMaking_Sim(object):
     def __init__(self):
         self._current = None
+        self._price_info = {}
+        self._attitude = ATTITUDE_TYPES.PESSMISTIC  # TODO maybe can be set by someway
+        self._slip_base = 0.2
         pass
+
+    @property
+    def slippage(self) -> float:
+        r = self._slip_base * (random.random() * 2 - 1 + 1)
+        return r if r < 1 else 1
 
     @property
     def current_time(self) -> datetime.datetime:
         return self._current
+
+    @property
+    def price_info(self) -> dict:
+        # The latest price info cache.
+        return self._price_info
+
+    @property
+    def attitude(self) -> ATTITUDE_TYPES:
+        return self._attitude
 
     def price_update(self, event: EventPriceUpdate):
         # TODO Check the source
@@ -26,3 +44,25 @@ class MatchMaking_Sim(object):
                 f"Current Time is {self.current_time} the price come from past {event.timestamp}"
             )
             return
+
+    def try_match(self, order_id: str):
+        # Get the order from db
+        # Get the price info from self.price_info
+        # If there is no detail about the code, Try get the data from db again.The store the info into self.price_info.
+        # According the price_info, try match the order.
+        # 1. If limit price
+        # 1.1 If the limit price is out of the bound of price_info, Cancel the order
+        # 1.1.1 Change the order status => CANCELED
+        # 1.1.2 Give it back to db
+        # 1.2 If the limit price > low and < high
+        # 1.2.1 Make it deal.
+        # 1.2.2 Change the order status => FILLED
+        # 1.2.3 Give it back to db
+        # 2. If Maket price
+        # 2.1 pessimistic
+        # 2.1.1 If buy, the fill price should be open + (high - open) * self.slippage
+        # 2.1.2 If sell, the fill price shoudl be open - (open - low) * self.slippage
+        # 2.2 optimistic
+        # 2.2.1 If buy, the fill price should be open - (open - low) * self.slippage
+        # 2.2.2 If sell, the fill price shoudl be open + (high - low) * self.slippage
+        pass
