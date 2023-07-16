085a0e7705d5f314b75ec6c8994e4a22c396117e
kaoru <809186577@qq.com>
2023-07-17 02:28:44 +0800

    Create sample cn daily update

--- samples/cn_share_update.py
+++
@@ -1,8 +0,0 @@
-from ginkgo.data.ginkgo_data import GinkgoData
-from ginkgo.data.models import *
-
-if __name__ == "__main__":
-    gd = GinkgoData()
-    gd.create_all()
-    gd.update_cn_codelist_to_latest_entire_async()
-    gd.update_bar_to_latest_entire_async()
