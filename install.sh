798ae694da2fa86c424ad106ffb36a79bf065b0d
Yu <suny@rc.inesa.com>
2023-10-17 17:08:46 +0800

    update install copy file, pass the parameters

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # cmd = f"sudo echo '{env}/bin/python {wd}/main.py' > /usr/bin/ginkgo_cli"
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-echo $SHELL_FOLDER
-sudo echo $SHELL_FOLDER/venv/bin/python $SHELL_FOLDER/main.py > /usr/bin/ginkgocli
+sudo echo $SHELL_FOLDER/venv/bin/python $SHELL_FOLDER/main.py \$@> /usr/bin/ginkgocli
 sudo chmod +x /usr/bin/ginkgocli
+
+echo "If you could see this under your command, it seems nothing wrong happend, you could type ginkgocli to use Ginkgo. Have Fun."
