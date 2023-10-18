016e45ce0da2047d41193f26edc6855d7606efc1
kaoru <809186577@qq.com>
2023-10-18 20:54:20 +0800

    update setup clean script.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # cmd = f"sudo echo '{env}/bin/python {wd}/main.py' > /usr/bin/ginkgo_cli"
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-sudo echo $SHELL_FOLDER/venv/bin/python $SHELL_FOLDER/main.py \$@> /usr/bin/ginkgocli
-sudo chmod +x /usr/bin/ginkgocli
+sudo rm /usr/bin/ginkgocli
+sudo echo $SHELL_FOLDER/venv/bin/python $SHELL_FOLDER/main.py \$@> /usr/bin/ginkgo
+sudo chmod +x /usr/bin/ginkgo
 
 echo "If you could see this under your command, it seems nothing wrong happend, you could type ginkgocli to use Ginkgo. Have Fun."
