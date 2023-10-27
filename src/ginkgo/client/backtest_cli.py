6f05d9641b2f33da2911846a9c40e71829efd6a3
Yu <suny@rc.inesa.com>
2023-10-27 14:54:24 +0800

    client update

--- src/ginkgo/client/backtest_cli.py
+++ src/ginkgo/client/backtest_cli.py
@@ -7,24 +7,42 @@ from rich.prompt import Prompt
 app = typer.Typer(help="Module for Backtest")
 
 
+class ResourceType(str, Enum):
+    backtest = "backtest"
+    strategy = "strategy"
+    selector = "selector"
+    sizer = "sizer"
+    risk_manager = "risk"
+    portfolio = "portfolio"
+    analyzer = "analyzer"
+    plot = "plot"
+
+
 @app.command()
-def list():
+def list(
+    resource: Annotated[ResourceType, typer.Argument(case_sensitive=False)],
+):
     """
-    Show strategy summary.
+    Show backtest summary.
     """
     pass
 
 
 @app.command()
-def run():
+def run(
+    id: Annotated[str, typer.Argument(case_sensitive=True)],
+):
     """
     Run Backtest.
     """
 
 
 @app.command()
-def edit():
+def edit(
+    resource: Annotated[ResourceType, typer.Argument(case_sensitive=False)],
+    id: Annotated[str, typer.Option(case_sensitive=True)],
+):
     """
-    Edit Strategy.
+    Edit Resources.
     """
     pass
