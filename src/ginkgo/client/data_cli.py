793c7c1408fc2931b9d671a81a02c4436dcea82d
Yu <suny@rc.inesa.com>
2023-10-30 17:16:57 +0800

    complete list show plt

--- src/ginkgo/client/data_cli.py
+++ src/ginkgo/client/data_cli.py
@@ -2,22 +2,84 @@ import typer
 from enum import Enum
 from typing_extensions import Annotated
 from rich.prompt import Prompt
+from rich.console import Console
 
 
 class DataType(str, Enum):
     STOCKINFO = "stockinfo"
     ADJUST = "adjust"
     DAYBAR = "day"
-    TICK = "min"
+    TICK = "tick"
+
+
+class PLTType(str, Enum):
+    DAYBAR = "day"
+    TICK = "tick"
 
 
 app = typer.Typer(help="Module for DATA")
+quit_list = ["NO", "N"]
+console = Console()
+
+
+def print_df_paganation(df, page: int):
+    if page > 0:
+        data_length = df.shape[0]
+        page_count = int(data_length / page) + 1
+        for i in range(page_count):
+            print(df[i * page : (i + 1) * page])
+            go_next_page = Prompt.ask(
+                f"Current: {(i+1)*page}/{data_length}, Conitnue? \[y/N]"
+            )
+            if go_next_page.upper() in quit_list:
+                console.print("See you soon. :sunglasses:")
+                raise typer.Abort()
+    else:
+        df = df.to_string()
+        print(df)
+
+
+@app.command()
+def plot(
+    data: Annotated[PLTType, typer.Argument(case_sensitive=False)],
+    code: Annotated[str, typer.Option(case_sensitive=False)] = "600000.SH",
+    start: Annotated[
+        str,
+        typer.Option(
+            case_sensitive=False,
+            help="Date Start, you could use yyyymmdd or yyyy-mm-dd",
+        ),
+    ] = "19900101",
+    end: Annotated[
+        str,
+        typer.Option(
+            case_sensitive=False,
+            help="Date End, you could use yyyymmdd or yyyy-mm-dd",
+        ),
+    ] = "21200001",
+):
+    from ginkgo.data.ginkgo_data import GDATA
+    from ginkgo.backtest.plots import CandlePlot
+
+    if data == DataType.DAYBAR:
+        info = GDATA.get_stock_info(code)
+        raw = GDATA.get_daybar_df(code, start, end)
+        code_name = info.code_name
+        industry = info.industry
+        df = GDATA.get_daybar_df(code, start, end)
+        plt = CandlePlot(f"[{industry}] {code} {code_name}")
+        plt.figure_init()
+        plt.update_data(df)
+        plt.show()
+    elif data == DataType.TICK:
+        # TODO How to plot ticks
+        pass
 
 
 @app.command()
 def list(
     data: Annotated[DataType, typer.Argument(case_sensitive=False)],
-    limit: Annotated[
+    page: Annotated[
         int, typer.Option(case_sensitive=False, help="Limit the number of output.")
     ] = 0,
     filter: Annotated[
@@ -33,9 +95,9 @@ def list(
 
     pd.set_option("display.unicode.east_asian_width", True)
 
-    if datatype == DataType.CODE:
+    if data == DataType.STOCKINFO:
         raw = GDATA.get_stock_info_df()
-        raw = raw[
+        rs = raw[
             [
                 "code",
                 "code_name",
@@ -44,31 +106,35 @@ def list(
                 "update",
             ]
         ]
-        print(raw.to_string())
-    elif datatype == DataType.DAYBAR:
+    elif data == DataType.DAYBAR:
         pass
-    elif datatype == DataType.TICK:
+    elif data == DataType.TICK:
         pass
+
+    print_df_paganation(rs, page)
 
 
 @app.command()
 def show(
     data: Annotated[DataType, typer.Argument(case_sensitive=False)] = "stockinfo",
-    code: Annotated[str, typer.Argument(case_sensitive=False)] = "600000.SH",
+    code: Annotated[str, typer.Option(case_sensitive=False)] = "600000.SH",
     start: Annotated[
         str,
         typer.Option(
             case_sensitive=False,
-            help="Date start, you could use yyyymmdd or yyyy-mm-dd",
+            help="Date Start, you could use yyyymmdd or yyyy-mm-dd",
         ),
-    ] = "20200101",
+    ] = "19900101",
     end: Annotated[
         str,
         typer.Option(
             case_sensitive=False,
-            help="Date start, you could use yyyymmdd or yyyy-mm-dd",
+            help="Date End, you could use yyyymmdd or yyyy-mm-dd",
         ),
-    ] = "20200201",
+    ] = "21200001",
+    page: Annotated[
+        int, typer.Option(case_sensitive=False, help="Limit the number of output.")
+    ] = 0,
 ):
     """
     Show data details.
@@ -82,7 +148,7 @@ def show(
 
     if data == DataType.STOCKINFO:
         raw = GDATA.get_stock_info_df(code=code)
-        raw = raw[
+        rs = raw[
             [
                 "code",
                 "code_name",
@@ -91,13 +157,12 @@ def show(
                 "update",
             ]
         ]
-        print(raw.to_string())
     elif data == DataType.DAYBAR:
-        df = GDATA.get_daybar_df(code, start, end)
-        print(df.to_string())
+        rs = GDATA.get_daybar_df(code, start, end)
     elif data == DataType.TICK:
-        pass
-    pass
+        raw = GDATA.get_tick_df(code, start, end)
+        rs = raw[["timestamp", "code", "price", "volume", "update", "direction"]]
+    print_df_paganation(rs, page)
 
 
 @app.command()
@@ -121,18 +186,25 @@ def update(
     Update the database.
     """
     from ginkgo.data.ginkgo_data import GDATA
-    from ginkgo.libs.ginkgo_logger import GLOG
 
-    GLOG.set_level("CRITICAL")
+    # from ginkgo.libs.ginkgo_logger import GLOG
+
+    # GLOG.set_level("CRITICAL")
 
     if data == DataType.STOCKINFO:
         GDATA.update_stock_info()
     elif data == DataType.ADJUST:
-        pass
+        GDATA.update_all_cn_adjustfactor_aysnc()
     elif data == DataType.DAYBAR:
-        pass
+        if code == "":
+            GDATA.update_all_cn_daybar_aysnc()
+        else:
+            GDATA.update_cn_daybar(code)
     elif data == DataType.TICK:
-        pass
+        if code == "":
+            GDATA.update_all_cn_tick_aysnc(fast_mode=fast)
+        else:
+            GDATA.update_tick(code, fast_mode=fast)
 
 
 @app.command()
