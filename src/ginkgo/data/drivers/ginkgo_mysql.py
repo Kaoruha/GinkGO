6514f79885446f4f4b69ca9e72192179cc34d021
Yu <suny@rc.inesa.com>
2023-11-02 16:45:40 +0800

    update ignore

--- src/ginkgo/data/drivers/ginkgo_mysql.py
+++ src/ginkgo/data/drivers/ginkgo_mysql.py
@@ -6,34 +6,55 @@ from ginkgo.libs.ginkgo_logger import GLOG
 
 class GinkgoMysql(object):
     def __init__(self, user: str, pwd: str, host: str, port: str, db: str) -> None:
-        self.engine = None
-        self.session = None
-        self.metadata = None
-        self.base = None
-        self.__user = user
-        self.__pwd = pwd
-        self.__host = host
-        self.__port = port
-        self.__db = db
+        self._engine = None
+        self._session = None
+        self._metadata = None
+        self._base = None
+        self._user = user
+        self._pwd = pwd
+        self._host = host
+        self._port = port
+        self._db = db
 
-        self.__connect()
+    @property
+    def engine(self):
+        if self._engine is None:
+            self.connect()
+        return self._engine
 
-    def __connect(self) -> None:
-        # uri = f"mysql+pymysql://{self.__user}:{self.__pwd}@{self.__host}:{self.__port}/{self.__db}?charset=uft8"
-        uri = f"mysql+pymysql://{self.__user}:{self.__pwd}@{self.__host}:{self.__port}/{self.__db}"
+    @property
+    def session(self):
+        if self._session is None:
+            self.connect()
+        return self._session
 
-        self.engine = create_engine(
+    @property
+    def metadata(self):
+        if self._metadata is None:
+            self.connect()
+        return self._metadata
+
+    @property
+    def base(self):
+        if self._base is None:
+            self.connect()
+        return self._base
+
+    def connect(self) -> None:
+        uri = f"mysql+pymysql://{self._user}:{self._pwd}@{self._host}:{self._port}/{self._db}"
+
+        self._engine = create_engine(
             uri,
             # pool_size=10,
             # pool_timeout=10,
             # max_overflow=5,
             # echo=True,
         )
-        self.session = sessionmaker(self.engine)()
-        self.metadata = MetaData(bind=self.engine)
+        self._session = sessionmaker(self.engine)()
+        self._metadata = MetaData(bind=self.engine)
         # self.base = declarative_base(metadata=self.metadata)
-        self.base = declarative_base()
-        GLOG.INFO("Connect to mysql succeed.")
+        self._base = declarative_base()
+        GLOG.DEBUG("Connect to mysql succeed.")
 
     @property
     def insp(self):
@@ -43,6 +64,5 @@ class GinkgoMysql(object):
         return self.insp.has_table(name)
 
     def get_table_size(self, model) -> int:
-        # count = self.session.query(func.count(model.uuid)).scalar()
         count = self.session.query(model).count()
         return count
