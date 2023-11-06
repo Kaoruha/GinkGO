cbea3395d43f59f43e36db9717fdacb107836d49
kaoru <809186577@qq.com>
2023-11-07 00:29:06 +0800

    client run seems ok, but some data seem not correct will cause the backtest stop

--- src/ginkgo/client/unittest_cli.py
+++ src/ginkgo/client/unittest_cli.py
@@ -43,6 +43,7 @@ def run(
     LOGGING_FILE_ON = GCONF.LOGGING_FILE_ON
     LOGGING_PATH = GCONF.LOGGING_PATH
     suite = unittest.TestSuite()
+    # origin_path = f"{GCONF.WORKING_PATH}/test"
     origin_path = f"{GCONF.WORKING_PATH}/test"
     path = []
 
