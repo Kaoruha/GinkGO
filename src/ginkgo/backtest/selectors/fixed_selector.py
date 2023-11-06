2ed48bb069166f6f3a631d8bc37e086c341d867e
Yu <suny@rc.inesa.com>
2023-11-06 17:49:17 +0800

    client run update

--- src/ginkgo/backtest/selectors/fixed_selector.py
+++ src/ginkgo/backtest/selectors/fixed_selector.py
@@ -2,11 +2,14 @@ from ginkgo.backtest.selectors.base_selector import BaseSelector
 
 
 class FixedSelector(BaseSelector):
+    abstract = False
+
     def __init__(
         self, codes: list, name: str = "FixedSelector", *args, **kwargs
     ) -> None:
         super(FixedSelector, self).__init__(name, *args, **kwargs)
         if not isinstance(codes, list):
+            print(codes)
             raise Exception("Codes must be list. Example: [code1,code2]")
         self._interested = codes
 
