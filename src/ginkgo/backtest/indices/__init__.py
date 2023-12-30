095c9f684eefa14fbbf646395523f7404b7fb8d2
kaoru <809186577@qq.com>
2023-12-31 06:09:17 +0800

    update client data plot function, support index

---
+++ src/ginkgo/backtest/indices/__init__.py
@@ -0,0 +1,5 @@
+from src.ginkgo.backtest.indices.simple_moving_average import SimpleMovingAverage
+from src.ginkgo.backtest.indices.weighted_moving_average import WeightedMovingAverage
+from src.ginkgo.backtest.indices.exponential_moving_average import (
+    ExponentialMovingAverage,
+)
---
+++ src/ginkgo/backtest/indices/simple_moving_average.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from src.ginkgo.backtest.indices.base_index import BaseIndex
+
+
+class SimpleMovingAverage(BaseIndex):
+    """
+    MA
+    """
+
+    def __init__(self, name: str = "", timeperiod: int = 5, *args, **kwargs) -> None:
+        super(SimpleMovingAverage, self).__init__(name, *args, **kwargs)
+        self._timeperiod = timeperiod
+
+    def cal(self, raw: pd.DataFrame, *args, **kwargs) -> None:
+        df = raw.copy()
+        rs = pd.DataFrame()
+        column_name = f"{self.name}"
+        rs["timestamp"] = df["timestamp"]
+        rs[column_name] = df["close"].rolling(self._timeperiod).mean()
+        # rs.fillna(0, inplace=True)
+        return rs
