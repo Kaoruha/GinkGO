080c1cfd1eb0f8c35bdfa593701de72cabd886db
kaoru <809186577@qq.com>
2024-01-08 02:08:43 +0800

    add index pinbar

--- src/ginkgo/backtest/indices/simple_moving_average.py
+++ src/ginkgo/backtest/indices/simple_moving_average.py
@@ -4,14 +4,14 @@ from src.ginkgo.backtest.indices.base_index import BaseIndex
 
 class SimpleMovingAverage(BaseIndex):
     """
-    MA
+    SMA
     """
 
-    def __init__(self, name: str = "", timeperiod: int = 5, *args, **kwargs) -> None:
+    def __init__(self, name: str = "sma", timeperiod: int = 5, *args, **kwargs) -> None:
         super(SimpleMovingAverage, self).__init__(name, *args, **kwargs)
         self._timeperiod = timeperiod
 
-    def cal(self, raw: pd.DataFrame, *args, **kwargs) -> None:
+    def cal(self, raw: pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
         df = raw.copy()
         rs = pd.DataFrame()
         column_name = f"{self.name}"
