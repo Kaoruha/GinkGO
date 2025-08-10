c6ffe6001aa870ece72c6edadaed03d10bef254d
kaoru <809186577@qq.com>
2025-08-10 19:53:13 +0800

    update backup script

--- backup.sh
+++ backup.sh
@@ -13,8 +13,10 @@ backup_file="${backup_dir}/ginkgo_backup_${current_time}.tar.gz"
 
 # 备份 ClickHouse 和 MySQL 数据目录到一个压缩包
 docker stop clickhouse_master
+docker stop mysql_master
 sudo tar --use-compress-program=pigz -cvf "$backup_file" --directory="./.db" mysql clickhouse
 docker start clickhouse_master
+docker start mysql_master
 
 # 打印完成消息
 echo "Backup completed: $backup_file"
